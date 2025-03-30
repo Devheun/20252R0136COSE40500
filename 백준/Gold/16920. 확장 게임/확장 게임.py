d90201fe0b3082ab02168c778aa1fc827160fea1
Devheun <86945971+Devheun@users.noreply.github.com>
2025-03-30 17:19:13 +0900

    [Gold II] Title: 확장 게임, Time: 504 ms, Memory: 197572 KB -BaekjoonHub

--- 백준/Gold/16920. 확장 게임/확장 게임.py
+++ 백준/Gold/16920. 확장 게임/확장 게임.py
@@ -1,21 +1,20 @@
 from collections import deque
 import sys
-input = sys.stdin.readline
+input=sys.stdin.readline
 
-N,M,P = map(int, input().split())
-S = [0] + list(map(int, input().split()))
+N,M,P = map(int,input().split())
+S = [0] + list(map(int,input().split()))
+castle = [deque() for _ in range(P+1)]
+ans = [0] * (P+1)
+
 board = [list(input()) for _ in range(N)]
 
-answer = [0] * (P+1)
-
-castle = [deque() for _ in range(P+1)]
-
-# 초기 성들 큐에 넣으면서 갯수 더해주기
+# 처음에 각각의 성들 큐에 넣기, 성 개수 기록하기
 for i in range(N):
     for j in range(M):
-        if board[i][j] != '.' and board[i][j] != '#': 
+        if board[i][j] != '.' and board[i][j] != '#':
+            ans[int(board[i][j])] += 1
             castle[int(board[i][j])].append((i,j))
-            answer[int(board[i][j])] += 1
 
 dx = (-1, 0, 1, 0)
 dy = (0, 1, 0, -1)
@@ -25,20 +24,22 @@ while True:
     for player in range(1, P+1):
         q = castle[player]
         for _ in range(S[player]):
-            if len(q) == 0:
+            if not q:
                 break
             for _ in range(len(q)):
                 curX, curY = q.popleft()
                 for d in range(4):
-                    nx = curX + dx[d]
-                    ny = curY + dy[d]
-                    
-                    if 0<=nx<N and 0<=ny<M and board[nx][ny]=='.':
-                        board[nx][ny] = str(player)
-                        answer[player] += 1
+                    nx = dx[d] + curX
+                    ny = dy[d] + curY
+                    if 0<=nx<N and 0<=ny<M and board[nx][ny] == '.':
+                        ans[player] += 1
                         q.append((nx,ny))
+                        board[nx][ny] = str(player)
                         flag = False
+                        
+            
     if flag:
         break
 
-print(*answer[1:])
+print(*ans[1:])
+       
