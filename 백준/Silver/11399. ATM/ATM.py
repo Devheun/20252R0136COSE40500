1028dc8d42f57947d64155dada6317d937cee825
Devheun <86945971+Devheun@users.noreply.github.com>
2025-03-30 18:51:31 +0900

    [Silver IV] Title: ATM, Time: 92 ms, Memory: 109544 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+P = list(map(int, input().split()))
+P.sort()
+
+ans = 0
+for i in range(N):
+    for j in range(i+1):
+        ans += P[j]
+print(ans)
