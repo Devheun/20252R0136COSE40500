357052fb19062925a3d773c044a9e3e0967a4813
Devheun <86945971+Devheun@users.noreply.github.com>
2025-04-26 00:46:23 +0900

    [level 2] Title: 기능개발, Time: 0.04 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,29 +1,23 @@
+import math
+
 def solution(progresses, speeds):
     days = []
     # 각 작업이 며칠 소요되는지 확인
-    for i in range(len(progresses)):
-        remain = 100 - progresses[i]
-        if remain % speeds[i] > 0:
-            days.append((remain//speeds[i]) + 1)
-        elif remain % speeds[i] == 0:
-            days.append((remain//speeds[i]))
+    for p,s in zip(progresses, speeds):
+        remain = 100 - p
+        days.append(math.ceil(remain / s))
     
     # 소요되는 일수 확인 후, stack의 top과 비교해서 소요되는 일수가 top보다 작으면 +1
-    # 7, 3, 9 (days)
     answer = []
-    cnt = 0
-    top = 0
-    for i in range(len(days)):
-        if i == 0:
+    cnt = 1
+    current = days[0]
+    for d in days[1:]:
+        if d <= current:
             cnt += 1
-            top = days[i]
-        elif top >= days[i]:
-            cnt += 1
-        elif top < days[i]:
+        else:
             answer.append(cnt)
             cnt = 1
-            top = days[i]
+            current = d
     
     answer.append(cnt)
-        
     return answer
