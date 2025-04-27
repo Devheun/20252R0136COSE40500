2a6690b040276e549c5aa5e86218b1dee1544f7a
Devheun <86945971+Devheun@users.noreply.github.com>
2025-04-27 16:48:11 +0900

    [level 2] Title: 프로세스, Time: 1.23 ms, Memory: 9.13 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,29 +1,16 @@
 from collections import deque
 
 def solution(priorities, location):
-    q = deque()
-    pq = deque(priorities)
-    answer = []
-    for i in range(len(priorities)):
-        q.append(i)
-    
-    while pq:
-        flag = False
-        curP = pq.popleft()
-        curI = q.popleft()
-        for i in pq:
-            if i > curP:
-                pq.append(curP)
-                q.append(curI)
-                flag = True
-                break
-        
-        if not flag:
-            answer.append(curI)
-    
-    for i in range(len(answer)):
-        if answer[i] == location:
-            return i+1
-    
+    q = deque([(i,p) for i,p in enumerate(priorities)])
+    answer = 0
+    while True:
+        cur = q.popleft()
+        if any(cur[1] < a[1] for a in q):
+            q.append(cur)
+        else:
+            answer += 1
+            if location == cur[0]:
+                return answer
+            
             
             
