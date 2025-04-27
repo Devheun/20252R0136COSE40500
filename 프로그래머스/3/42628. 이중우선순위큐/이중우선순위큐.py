01aaf47ac5f506b21eead9b20979fc2d55c16848
Devheun <86945971+Devheun@users.noreply.github.com>
2025-04-28 00:13:45 +0900

    [level 3] Title: 이중우선순위큐, Time: 87.78 ms, Memory: 18.8 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,23 +1,42 @@
 import heapq
+from collections import Counter
 
 def solution(operations):
-    answer = []
-    heap = []
+    min_h, max_h = [], []
+    cnt = Counter()
+
     for oper in operations:
-        c, num = oper.split()
-        if c == 'I':
-            heapq.heappush(heap, int(num))
-        elif heap:
-            if c == 'D' and int(num) == 1:
-                max_val = max(heap)
-                heap.remove(max_val)
-            elif c == 'D' and int(num) == -1:
-                heapq.heappop(heap)
-                
-    
-    if not heap:
-        answer = [0,0]
-    else:
-        answer = [max(heap), heapq.heappop(heap)]
-            
-    return answer
+        op, num = oper.split()
+        n = int(num)
+
+        if op == 'I':
+            cnt[n] += 1
+            heapq.heappush(min_h, n)
+            heapq.heappush(max_h, -n)
+
+        else:  # 'D'
+            if n == 1:
+                # 최대값 삭제
+                while max_h and cnt[-max_h[0]] == 0:
+                    heapq.heappop(max_h)
+                if max_h:
+                    v = -heapq.heappop(max_h)
+                    cnt[v] -= 1
+            else:
+                # 최솟값 삭제
+                while min_h and cnt[min_h[0]] == 0:
+                    heapq.heappop(min_h)
+                if min_h:
+                    v = heapq.heappop(min_h)
+                    cnt[v] -= 1
+
+    # 남아 있는 stale 원소 정리
+    while min_h and cnt[min_h[0]] == 0:
+        heapq.heappop(min_h)
+    while max_h and cnt[-max_h[0]] == 0:
+        heapq.heappop(max_h)
+
+    # 결과 반환
+    if not min_h:
+        return [0, 0]
+    return [-max_h[0], min_h[0]]
