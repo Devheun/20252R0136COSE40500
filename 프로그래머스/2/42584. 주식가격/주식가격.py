f5a38902777d72bf81b40e083a38f66891ff8141
Devheun <86945971+Devheun@users.noreply.github.com>
2025-04-27 18:00:17 +0900

    [level 2] Title: 주식가격, Time: 33.65 ms, Memory: 19.2 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,19 +1,15 @@
 def solution(prices):
     answer = []
+    length = len(prices)
+    # 모든 가격 max로 세팅
+    answer = [i for i in range(length-1, -1, -1)]
     
-    for i in range(len(prices) - 1):
-        cur = prices[i]
-        flag = False
-        for j in range(i+1, len(prices)):
-            if cur > prices[j]:
-                answer.append(j - i)
-                flag = True
-                break
-        if flag:
-            continue
-        else:
-            answer.append(len(prices) - i - 1)
-    
-    answer += [0]
+    stack = [0]
+    for i in range(1, length):
+        while stack and prices[stack[-1]] > prices[i]: # 값이 떨어지는 지점이 생기는 경우
+            j = stack.pop()
+            answer[j] = i - j
         
+        stack.append(i)
+
     return answer
