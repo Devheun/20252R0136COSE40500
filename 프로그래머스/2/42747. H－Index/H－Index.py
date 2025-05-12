cc33d6f92928534b83489e57d8014f7c26952af1
Devheun <86945971+Devheun@users.noreply.github.com>
2025-05-12 23:32:52 +0900

    [level 2] Title: H-Index, Time: 0.26 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,26 +1,9 @@
-def search(arr):
-    h_index = 0
-    for i in range(max(arr)):
-        h_temp = i
-        cnt_high = 0
-        cnt_low = 0
-        for j in range(len(arr)):
-            if arr[j] >= h_temp:
-                cnt_high += 1
-            elif arr[j] < h_temp:
-                cnt_low += 1
-        
-        if cnt_high >= h_temp and cnt_low <= h_temp:
-            h_index = h_temp
-        else:
-            break
-    
-    return h_index
-                
-        
-    
 def solution(citations):
+    citations.sort(reverse=True)
+    h_index = 0
     answer = 0
-    citations.sort()
-    answer = search(citations)
+    for i, c in enumerate(citations):
+        if c >= i+1:
+            h_index = i + 1
+            answer = h_index
     return answer
